f3745ba113d209bc8bd089c4372bcf55cf752c5c
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-09-04 02:03:53 -0500

    Added functionality to display temperature upon location input

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,34 @@
 
 var apiKey = "38a07745366c739b58fb8517656acd34";
 
-var city;
-
 // var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
 function getAPI() {
 
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var citySearch = document.getElementById('city-search');
+    var city = citySearch.value;
+
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
   
     fetch(queryUrl)
-      .then(function (response) {
-        return response.json();
+        .then(function (response) {
+            return response.json();
       })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('li');
-          listItem.textContent = data[i].html_url;
-          repoList.appendChild(listItem);
-        }
+        
+        .then(function (data) {
+            var cityTemp = document.getElementById('city-temp');
+            cityTemp.innerHTML = data.main.temp;
       });
   }
 
 var submitBtn = document.getElementById('submit');
 submitBtn.addEventListener('click', getAPI);
+
+
+
+
+// for (var i = 0; i < data.length; i++) {
+//     var listItem = document.createElement('li');
+//     listItem.textContent = data[i].html_url;
+//     repoList.appendChild(listItem);
+//   }
